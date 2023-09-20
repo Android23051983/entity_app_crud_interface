fdaecffc2277cb4ea8e776ea1bc53de34498092e
Andrei <ikar2008@hotmail.com>
2023-09-20 15:11:16 +0300

    Добавьте файлы проекта.

---
+++ entity_app_crud_interface/AddHeroDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace entity_app_crud_interface
+{
+    public static class AddHeroDB
+    {
+        public static void AddAll(Hero hero)
+        {
+            using (AppContext db = new AppContext())
+            {
+                Hero h = db.Heroes.Where(c => c.Name == hero.Name && c.Race == hero.Race && c.Age == hero.Age && c.Weapon == hero.Weapon).FirstOrDefault()!;
+                if (h == null) 
+                { 
+                    db.Heroes.Add(hero);
+                    db.SaveChanges();
+                    MessageBox.Show("Новый герой добавлен в БД");
+                }
+                else
+                {
+                    MessageBox.Show("Такой герой уже есть в БД. Добавьте другого героя, введя другие данные!!!");
+                }
+            }
+        }
+
+        public static void AddName(Hero hero)
+        {
+            using (AppContext db = new AppContext())
+            {
+                Hero h = db.Heroes.Where(c => c.Name == hero.Name).FirstOrDefault()!;
+                if (h == null)
+                {
+                    db.Heroes.Add(hero);
+                    db.SaveChanges();
+                    MessageBox.Show("Новый герой добавлен в БД");
+                }
+                else
+                {
+                    MessageBox.Show("Такой герой уже есть в БД. Добавьте другого героя, введя другие данные!!!");
+                }
+            }
+        }
+    }
+}
---
+++ entity_app_crud_interface/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_app_crud_interface
+{
+    public class AppContext : DbContext
+    {
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+
+
+        public DbSet<Hero> Heroes { get; set; } = null!;
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=LotR_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
